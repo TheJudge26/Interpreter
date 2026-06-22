b83bf528fbcfba00c062a0ef99475fb697f4c739
Danial Awny <danialawny26@gmail.com>
2026-06-22 18:12:51 +0300

    can do + or - for a single op like 1+1

--- inter.c
+++ inter.c
@@ -1,33 +1,141 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define input_len 1000
 
-void interpret(char *text)
-{
-char c;
+#define INT 0
+#define OP 1
+#define err -1
+#define INT_STR "INT"
+#define OPERATOR_STRING "OPERATOR"
 
-while ((c=*text)!='\0')
+
+typedef struct 
 {
-    printf("%c\n",c);
-    text++;
+    int type;//num or op 2+2
+    char operator_value;
+    int number_value;
+
+}Token;
+
+int current_pos=0;
+
+Token current_token;
+
+bool is_digit(char c)
+{
+   return c>='0'&&c<='9';
+    
 }
 
+bool is_operator(char c)
+{
+   return c=='+'||c=='-';
+    
+}
 
+void get_next_token(char *text){
+
+    char c = text[current_pos];
+
+    if (is_digit(c))
+    {
+        current_token.type=INT;
+
+        current_token.number_value=c-'0';
+        
+        current_token.operator_value=' ';
+    }
+    
+    else if(is_operator(c)) 
+    {
+        current_token.type=OP;
+
+        current_token.operator_value=c; 
+
+        current_token.number_value=0;
+    }
+    else
+    {
+        // current_token.type = err;
+        // current_token.number_value=0;
+        printf("invalid token at pos %d : %c\n",current_pos,c);
+        exit(-1);
+    }
+    current_pos ++ ;
+}
+
+void token_str(Token token, char *s) // token as string in string s
+{
+    // char *type = OPERATOR_STRING;
+    // if (token.type==INT)
+    // {
+    //     type = INT_STR;
+    // }
+    // char *value = token.number_value;
+    // if (token.type==OP)
+    // {
+    //     value = &token.operator_value;
+    // }
+    
+    sprintf(s,"Token (type=%s, numper_value=%d,operator_vlaue=%c)", token.type==INT? INT_STR:OPERATOR_STRING,token.number_value,token.operator_value);
+}
+
+void parse(int type){
+
+    if (current_token.type!=type)
+    {
+        char s[100];
+        token_str(current_token, s);
+        printf("syntax error at pos %d: Expected token of type %s,but received tosken%s", current_pos,type==INT ? INT_STR : OPERATOR_STRING,s);
+        exit(-1);
+    }
+    
+}
+
+int interpret(char *text)//from the text to tokens 
+{
+    get_next_token(text);
+    parse(INT);
+    int left = current_token.number_value;
+
+    get_next_token(text);
+    parse(OP);
+    int operator = current_token.operator_value;
+
+    get_next_token(text);
+    parse(INT);
+    int right = current_token.number_value;
+
+    current_pos=0;
+
+    int result;
+
+    if (operator=='+')
+    {
+        result = left + right;
+    }
+    else{
+        result=left-right;
+    }
+    return result;
 }
 int main() {
 
 char s[input_len];
 printf("Interpreter is running......");
+
+int result;
 while (true)
 {
     printf(">>>");
 
     fgets(s,input_len,stdin);
 
-    interpret(s);
+    result =interpret(s);
 
-
+    printf("%d\n",result);
 }
 
     
